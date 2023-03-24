32b13d4fbdd0cbf1e72b96121a2d0bfec4fb668a
evan <95433637+evankagarise@users.noreply.github.com>
2023-03-24 09:02:40 -0400

    added rate limit

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -8,7 +8,14 @@ import { createTRPCRouter, privatProedure, publicProcedure } from "~/server/api/
 const filterUserForClient = (user: User) => {
   return {id: user.id, username: user.username, profilePicture: user.profileImageUrl};
 }
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
 
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(3, "1 m"),
+  analytics: true
+})
 export const postsRouter = createTRPCRouter({
 
   getAll: publicProcedure.query(async ({ ctx }) => {
@@ -43,6 +50,9 @@ export const postsRouter = createTRPCRouter({
   })).mutation(async ({ctx, input}) => {
     const authorId = ctx.userId;
 
+    const {success} = await ratelimit.limit(authorId)
+
+    if (!success) throw new TRPCError({code: "TOO_MANY_REQUESTS"})
     const post = await ctx.prisma.post.create({
       data: {
         authorId,
